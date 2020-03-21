358fa4ecc8807aae91401c1fb9dd2f4c2bf8e00f
Ye Lu <yelu510@gmail.com>
2020-03-21 12:40:04 -0700

    user fixing4

--- app/routers/user.router.js
+++ app/routers/user.router.js
@@ -1,26 +1,35 @@
-module.exports = (app, router) => {
-    const userController = require('../controllers/user.controller.js');
+// module.exports = (app, router) => {
+//     const userController = require('../controllers/user.controller.js');
 	
-	var path = __basedir + '/views/';
+// 	var path = __basedir + '/views/';
 	
-	router.use((req,res,next) => {
-		console.log("/" + req.method);
-		next();
-	});
+// 	router.use((req,res,next) => {
+// 		console.log("/" + req.method);
+// 		next();
+// 	});
 	
-	app.get('/', (req,res) => {
-		res.sendFile(path + "index.html");
-	});
+// 	app.get('/', (req,res) => {
+// 		res.sendFile(path + "index.html");
+// 	});
 	
-	//app.post('/api/files/upload', upload.single("uploadfile"), fileController.uploadFile);
+// 	//app.post('/api/files/upload', upload.single("uploadfile"), fileController.uploadFile);
 	
-	app.get('/api/users/getall', userController.listAllUsers);
+// 	app.get('/api/users/getall', userController.listAllUsers);
 	
-	//app.get('/api/users/:id', userController.downloadFile);
+// 	//app.get('/api/users/:id', userController.downloadFile);
 
-	app.use('/',router);
+// 	app.use('/',router);
  
-	app.use('*', (req,res) => {
-		res.send("path not found");
-	});
-}
+// 	app.use('*', (req,res) => {
+// 		res.send("path not found");
+// 	});
+// }
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+	res.send('USERS')
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ db.sequelize.sync({force: false}).then(() => {
 //app.use(express.static('resources'));	
 
 require('./app/routers/file.router.js')(app, router, upload);
-require('./app/routers/user.router.js')(app, router);
+app.use('/users', require('./app/routers/user.router.js'));
  
 // Create a Server
 var server = app.listen(3000, function () {
