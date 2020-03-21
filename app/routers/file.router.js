6488eb3015fdf4866c2d346f5a8da25bd9e808a3
Ye Lu <yelu510@gmail.com>
2020-03-21 14:33:16 -0700

    user fixing13

--- app/routers/file.router.js
+++ app/routers/file.router.js
@@ -1,29 +1,40 @@
 
 
-module.exports = (app, router, upload) => {
+// module.exports = (app, router, upload) => {
 
-    const fileController = require('../controllers/file.controller.js');
+//     const fileController = require('../controllers/file.controller.js');
 	
-	var path = __basedir + '/views/';
+// 	var path = __basedir + '/views/';
 	
-	router.use((req,res,next) => {
-		console.log("/" + req.method);
-		next();
-	});
+// 	router.use((req,res,next) => {
+// 		console.log("/" + req.method);
+// 		next();
+// 	});
 	
-	app.get('/', (req,res) => {
-		res.sendFile(path + "index.html");
-	});
+// 	app.get('/', (req,res) => {
+// 		res.sendFile(path + "index.html");
+// 	});
 	
-	app.post('/api/files/upload', upload.single("uploadfile"), fileController.uploadFile);
+// 	app.post('/api/files/upload', upload.single("uploadfile"), fileController.uploadFile);
 	
-	app.get('/api/files/getall', fileController.listAllFiles);
+// 	app.get('/api/files/getall', fileController.listAllFiles);
 	
-	app.get('/api/files/:id', fileController.downloadFile);
+// 	app.get('/api/files/:id', fileController.downloadFile);
 
-	app.use('/',router);
+// 	app.use('/',router);
  
-	app.use('*', (req,res) => {
-		res.sendFile(path + "404.html");
-	});
-}
+// 	app.use('*', (req,res) => {
+// 		res.sendFile(path + "404.html");
+// 	});
+// }
+
+const express = require('express');
+const router = express.Router();
+const fileController = require('../controllers/file.controller.js');
+
+
+router.get('/files/getall', fileController.listAllFiles);
+
+
+
+module.exports = router;
