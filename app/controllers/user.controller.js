e8fd92b4bd9e029ef65cecb649cc0a3ce34dd930
Ye Lu <yelu510@gmail.com>
2020-03-21 12:17:38 -0700

    user fixing1

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -5,6 +5,7 @@ const User = db.users;
 
 
 exports.listAllUsers = (req, res) => {
+	console.log("listallUsers was called****")
 	User.findAll({attributes: ['id']}).then(users => {
 	  res.json(users);
 	});
