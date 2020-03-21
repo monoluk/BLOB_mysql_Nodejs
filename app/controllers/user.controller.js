cf13a902a2a6bcbba35ba7a2fb7d7e28359ad7e3
Ye Lu <yelu510@gmail.com>
2020-03-21 14:01:25 -0700

    user fixing10

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -5,8 +5,15 @@ const User = db.users;
 
 
 exports.listAllUsers = (req, res) => {
-	console.log("listallUsers was called****")
 	User.findAll({attributes: ['id','username','useremail','userpassword','blocked','createdAt','updatedAt']}).then(users => {
 	  res.json(users);
 	});
+}
+
+exports.searchUser = (req, res) =>{
+
+	File.findById(req.params.id).then(user => {
+		res.json(user);
+	})
+
 }
