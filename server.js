4af7f82c979ff26f066b56bf97fb2b8cb211dc6c
Ye Lu <yelu510@gmail.com>
2020-03-21 12:51:19 -0700

    user fixing6

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ db.sequelize.sync({force: false}).then(() => {
 
 //app.use(express.static('resources'));	
 
-require('./app/routers/file.router.js')(app, router, upload);
-app.use('/users', require('./app/routers/user.router'));
+//require('./app/routers/file.router.js')(app, router, upload);
+app.use('/users', require('./app/routers/user.router.js'));
  
 // Create a Server
 var server = app.listen(3000, function () {
