a1921fb531a7062aebe64873da990ce2a8704cfb
Ye Lu <yelu510@gmail.com>
2020-03-21 12:30:53 -0700

    user fixing3

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var router = express.Router();
 var upload = require('./app/config/multer.config.js');
@@ -6,6 +7,16 @@ var upload = require('./app/config/multer.config.js');
 global.__basedir = __dirname;
  
 const db = require('./app/config/db.config.js');
+
+//address cross-server issue 
+app.use(cors({
+	origin: function(origin, callback){
+	  return callback(null, true);
+	},
+	optionsSuccessStatus: 200,
+	credentials: true
+  }));
+
   
 // force: true will drop the table if it already exists
 db.sequelize.sync({force: false}).then(() => {
@@ -16,10 +27,9 @@ db.sequelize.sync({force: false}).then(() => {
 
 require('./app/routers/file.router.js')(app, router, upload);
 require('./app/routers/user.router.js')(app, router);
-
  
 // Create a Server
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
  
   var host = server.address().address
   var port = server.address().port
