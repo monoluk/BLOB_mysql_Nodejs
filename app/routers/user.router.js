2e2dab8086d8b921d755cec9bdf7e12c428ee676
Ye Lu <yelu510@gmail.com>
2020-03-21 12:54:27 -0700

    user fixing7

--- app/routers/user.router.js
+++ app/routers/user.router.js
@@ -27,6 +27,7 @@
 
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user.controller.js');
 
 router.get('/', (req, res)=>{
 	console.log('User route is called')
